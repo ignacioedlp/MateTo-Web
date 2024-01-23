a176b714e08a640f41f74caec79dfc3a423fbb0e
Ignacio Cafiero Torrubia <ignaciocafiero12@gmail.com>
2024-01-23 17:55:36 -0300

    Remove storybook

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
